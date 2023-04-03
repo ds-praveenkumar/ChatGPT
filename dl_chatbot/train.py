8eb99034b03ef8c1957ebb51df96ed28f12c15d1
ds-praveenkumar <praveenkr2208@gmail.com>
2023-04-03 13:40:16 +0530

    add sinple dl chatbot

--- dl_chatbot/train.py
+++ dl_chatbot/train.py
@@ -8,17 +8,19 @@ from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 from sklearn.preprocessing import LabelEncoder
 
-with open('dataset.json') as file:
+with open('./dl_chatbot/dataset.json') as file:
     data = json.load(file)
-    
+
 training_sentences = []
 training_labels = []
 labels = []
 responses = []
 
+print( data )
 
 for intent in data['intents']:
     for pattern in intent['patterns']:
+        print( pattern)
         training_sentences.append(pattern)
         training_labels.append(intent['tag'])
     responses.append(intent['responses'])
@@ -53,3 +55,21 @@ model.compile(loss='sparse_categorical_crossentropy',
               optimizer='adam', metrics=['accuracy'])
 
 model.summary()
+
+epochs = 500
+history = model.fit(padded_sequences, np.array(training_labels), epochs=epochs)
+
+# to save the trained model
+model.save("chat_model")
+
+import pickle
+
+# to save the fitted tokenizer
+with open('tokenizer.pickle', 'wb') as handle:
+    pickle.dump(tokenizer, handle, protocol=pickle.HIGHEST_PROTOCOL)
+    
+# to save the fitted label encoder
+with open('label_encoder.pickle', 'wb') as ecn_file:
+    pickle.dump(lbl_encoder, ecn_file, protocol=pickle.HIGHEST_PROTOCOL)
+
+
